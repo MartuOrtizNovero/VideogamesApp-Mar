e40fe9910afc4ba2eb02f3e7e89ca6fa2a7160a9
Martina Ortiz Novero <martuortiznov@gmail.com>
2022-08-22 16:46:17 -0300

    cambios

--- api/index.js
+++ api/index.js
@@ -19,10 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+
 //3001
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log('%s listening at'+ process.env.PORT); // eslint-disable-line no-console
   });
 });
